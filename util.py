301f6d94616326dbad82305dc6e8af2be4690929
Beel Sarwar <ns3429@nyu.edu>
2019-05-02 22:39:17 -0400

    minor fix

--- util.py
+++ util.py
@@ -39,12 +39,9 @@ def closest_k_neighbor(x, X, K = 7 ):
     index_of_k_closest = distance_arg_sorted[K-1]
     return X[index_of_k_closest, :]
 
-
 def run_self(X, y, n_components, beta = 0.5):    
     mask = np.zeros(X.shape[0])
-    size = np.floor(np.min([X.shape[0] * .1, 200]))
-
-    take_these_label_indices = np.random.choice(X.shape[0], size=int(size), replace = False)
+    take_these_label_indices = np.random.choice(X.shape[0], size= 200, replace = False)
     mask[take_these_label_indices] = 1
     X_labeled = X[mask.astype(bool), :]
     y_labeled = y[mask.astype(bool)]
@@ -53,7 +50,7 @@ def run_self(X, y, n_components, beta = 0.5):
     neighbors = np.apply_along_axis(closest_k_neighbor, 1, X_labeled, X = X_all)
     sigmas = np.apply_along_axis(lambda x: np.sqrt(np.sum(x**2)), 1, X_labeled - neighbors)
     
-
+    
     n_labeled = X_labeled.shape[0]
     n_all = X.shape[0]
     
@@ -65,29 +62,31 @@ def run_self(X, y, n_components, beta = 0.5):
         for jx in np.arange(n_labeled):
             x_i = X_labeled[ix, :]
             x_j = X_labeled[jx, :]
-            a_matrix[ix, jx] = np.exp(-np.sum((x_i - x_j)**2) / sigmas[ix] / sigmas[jx])
 
             if (y_labeled[ix] == y_labeled[jx]):
+                a_matrix[ix, jx] = np.exp(-np.sum((x_i - x_j)**2) / sigmas[ix] / sigmas[jx])
                 n_y = np.sum(y_labeled == y_labeled[ix])
                 w_lb[ix, jx] = a_matrix[ix, jx] * (1.0 / n_labeled - 1.0 / n_y)
-                w_lw[ix, jx] = a_matrix[ix, jx] * (1.0 / n_labeled)
+                w_lw[ix, jx] = a_matrix[ix, jx] * (1.0 / n_y)
             else:
                 w_lb[ix, jx] = (1.0 / n_labeled)
 
     Xprime = np.transpose(X_labeled)
     mu = X_all.mean(axis = 0)
-    S_lb = Xprime @ (np.diag(np.matmul(w_lb, 1.0 * np.ones(n_labeled) / n_labeled)) - w_lb) @ X_labeled      
-    S_lw = Xprime @ (np.diag(np.matmul(w_lw, 1.0 * np.ones(n_labeled) / n_labeled)) - w_lw) @ X_labeled  
+    S_lb = (Xprime.dot(np.diag(w_lb.mean(axis = 0)) - w_lb)).dot(X_labeled)      
+    S_lw = (Xprime.dot(np.diag(w_lw.mean(axis = 0)) - w_lw)).dot(X_labeled)  
 
-    S_t = np.transpose(X_all).dot(X_all) - n_all * mu.reshape(-1, 1) @ mu.reshape(1, -1)
+
+    S_t = np.transpose(X_all).dot(X_all) - n_all * mu.reshape(-1, 1).dot(mu.reshape(1, -1))
 
     S_rlb = (1 - beta) * S_lb + beta * S_t
     S_rlw = (1 - beta) * S_lw + beta * np.eye(d)
 
+
     eig, vectors = scipy.linalg.eig(a = S_rlb, b = S_rlw)
     eig = np.real(eig)
     vectors = np.real(vectors)
-    eig = np.sqrt(eig[0:n_components])
+    eig = np.sqrt(eig[:n_components])
     vectors = vectors[:, :n_components]
     T = np.zeros((d, n_components))
     for i in range(n_components):
@@ -95,7 +94,9 @@ def run_self(X, y, n_components, beta = 0.5):
     
     return T
 
+
 def run_neighbor_classifier(ncomponents, train_data, test_data, train_labels, test_labels, run = None, seed = 3429):
+    np.random.seed(seed)
     start_time = time.time()
     if run == "pca":
         pca_model = PCA(n_components=ncomponents)
